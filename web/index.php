1ac862202443aedd4c25a463f7d7d7d07f1e5a23
Ciobotariu Andrei <58783143+UltraTDK@users.noreply.github.com>
2022-04-13 14:47:18 +0300

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/homepage.css" rel="stylesheet">
+    <title>Site name</title>
+</head>
+
+<body>
+
+    <header class="site_header">
+        <nav class="navig_line">
+            <div class="site_name">
+                <a href="/homepage.html">Site name</a>
+            </div>
+            <form action="/homepage.html" method="get">
+                <div class="search_bar">
+                    <input type="search" id="search" name="search" placeholder=" Search...">
+                </div>
+            </form>
+            <ul class="container">
+                <li>
+                    <div class="latest">
+                        <a href="homepage.html">Latest</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="categories">
+                        <a href="#Categories">Categories</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="about">
+                        <a href="/about.html">About</a>
+                    </div>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- <div class="news">
+            <p>Check out our latest post here!</p>
+        </div>-->
+    </header>
+    <main>
+        <section class="main_posts">
+            Main posts here <br>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit blanditiis atque perferendis ab earum sed.
+            Nostrum cum voluptatibus numquam consequatur similique dicta facilis doloribus, ipsam provident fugit
+            expedita, sunt ratione!
+            Esse est ad adipisicing culpa aute dolore mollit esse id incididunt nisi et sunt. Reprehenderit amet
+            incididunt sit adipisicing nulla deserunt enim. Incididunt in ipsum ex minim adipisicing ad aute nulla.
+            Culpa culpa consequat cillum elit cillum non velit exercitation pariatur fugiat proident excepteur irure.
+            Lorem cupidatat sit ullamco laborum magna ex aliqua minim.
+
+            In quis ut sunt fugiat labore deserunt nisi esse esse ad labore. Officia nulla consequat in tempor
+            consectetur eu exercitation laborum duis labore dolore. Lorem nulla irure minim nulla minim.
+            <hr>
+        </section>
+
+        <section class="infos">
+            Infos <br>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero tenetur eveniet ratione saepe animi totam eum
+            laborum repellat, dolorum quasi vitae quidem, rem ea ullam ex explicabo quis doloribus iure?
+            <hr>
+        </section>
+        <section class="categories" id="Categories">
+            Categories <br>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, soluta. Placeat veniam cumque incidunt,
+            voluptas fugit cum molestias nulla tempora non laborum commodi soluta corporis tenetur eius ratione minus
+            harum.
+        </section>
+    </main>
+
+    <footer>
+        <p>Footer infos</p>
+    </footer>
+
+</body>
+
+</html>
