3a78dff9602a4f7d1f910b84f63a120e35096e3e
Vasilescu Stefan-Octavian[E4] <stefanoctavianvasilescu@gmail.com>
2022-05-03 09:42:03 +0300

    Added placeholders for comment crawling

---
+++ Web-Crawler/python/src/youtube_comments.py
@@ -0,0 +1,16 @@
+import datetime
+from typing import Any
+from utils.DBManager import DBManager
+from requester import Requester
+
+
+class YoutubeComments(Requester):
+    def __init__(self, db: DBManager) -> None:
+        super(YoutubeComments, self).__init__(db)
+        self.db.set_table_name("youtube_comments")
+
+    def request(self, query: Any) -> list[dict[str, str | int | datetime]]:
+        return super().request(query)
+
+    def send_to_db(self, data: list[dict[str, str | int | datetime]]) -> None:
+        return
