441e38967aee273298858939eb9b36855ab18bc0
Ciobotariu Andrei <ciobotariuandrei@yahoo.com>
2022-05-07 17:29:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/styles/general.css" rel="stylesheet">
-    <link href="/styles/homepage.css" rel="stylesheet">
+    <link href="styles/general.css" rel="stylesheet">
+    <link href="styles/homepage.css" rel="stylesheet">
     <script src="scripts/responsive.js" defer></script>
     <script src="scripts/darktheme.js" defer></script>
     <script src="scripts/homepage.js" defer></script>
