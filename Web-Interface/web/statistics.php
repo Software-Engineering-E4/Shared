e90c911cc86d31616ac1c626b57adbe9ea0362bd
RalucaMunteanuFlorea21 <79167533+RalucaMunteanuFlorea21@users.noreply.github.com>
2022-05-08 21:17:29 +0300

    Add files via upload

--- Web-Interface/web/statistics.php
+++ Web-Interface/web/statistics.php
@@ -1,7 +1,6 @@
 <?php
     require "dbconnection.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,8 +12,9 @@
     <link href="styles/statistics.css" rel="stylesheet">
     <script src="scripts/responsive.js" defer></script>
     <script src="scripts/darktheme.js" defer></script>
-    <script src="scripts/homepage.js" defer></script>
-    <title>Statistics</title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+    <title>Site name</title>
 </head>
 
 <body>
@@ -35,7 +35,7 @@
                     <a class="menu_option" href="homepage.html"> Latest </a>
                 </li>
                 <li class="categories">
-                    <a class="menu_option" href="#Categories">Categories</a>
+                    <a class="menu_option" href="homepage.html#Categories">Categories</a>
                 </li>
                 <li class="statistics">
                     <a class="menu_option" href="statistics.html">Statistics</a>
@@ -63,11 +63,48 @@
     </header>
     <main>
         <div class="statistics">
-            <p class="first_statistics">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum explicabo
-                maxime animi eveniet! Ipsam distinctio perferendis iste a fugit consectetur asperiores omnis
-                nesciunt
-                architecto earum pariatur, tenetur placeat cupiditate doloribus.
-            </p>
+            <h2 class="twitter">Twitter</h2>
+            <div class="twitter">
+                <div class="chart">
+                    <ul class="values">
+                        <!--vertical-->
+                        <li><span>100%</span></li>
+                        <li><span>50%</span></li>
+                        <li><span>0%</span></li>
+                    </ul>
+                    <ul class="bars">
+                        <li>
+                            <div class="bar" data-percentage="70"><span>Negative</span></div>
+                        </li>
+                        <li>
+                            <div class="bar" data-percentage="20"><span>Positive</span></div>
+                        </li>
+                        <li>
+                            <div class="bar" data-percentage="30"><span>Neutral</span></div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <h2 class="reddit">Reddit</h2>
+            <div class="reddit">
+
+            </div>
+            <h2 class="youtube">Youtube</h2>
+            <div class="youtube">
+
+            </div>
+            <!--script desenare bare-->
+            <script type="text/javascript">
+                $(function () {
+                    $('.bars li .bar').each(function (key, bar) {
+                        var percentage = $(this).data('percentage');
+                        $(this).animate({
+                            'height': percentage + '%'
+                        }, 1000);
+                    });
+                });
+            </script>
         </div>
     </main>
+    </html>
